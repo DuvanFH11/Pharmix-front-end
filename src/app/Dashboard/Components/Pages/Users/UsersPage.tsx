2cd7842e794ec02e39bb143cc942262da96e45d5
Duvan Florez <duvanfh11@gmail.com>
2026-08-29 22:01:40 -0500

    Se crean las páginas principales para iniciar su desarrollo

---
+++ src/app/Dashboard/Components/Pages/Users/UsersPage.tsx
@@ -0,0 +1,8 @@
+const UsersPages = () => {
+    return (
+        <>
+            <h1>Users Pages</h1>
+        </>
+    )
+}
+export default UsersPages;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,6 +3,9 @@ import { ProtectedRoute } from "../components/protectedRoute";
 import Login from "../app/Access/Login";
 import Register from "../app/Access/Register";
 import Dashboard from "../app/Dashboard/Dashboard";
+import DashboardPage from "../app/Dashboard/Components/Pages/Dashboard/DashboardPage";
+import UsersPages from "../app/Dashboard/Components/Pages/Users/UsersPage";
+import ProductsPage from "../app/Dashboard/Components/Pages/Products/ProductsPage";
 const AppRoutes = () => {
     return (
         <>
@@ -11,7 +14,11 @@ const AppRoutes = () => {
                 <Route path="/register" element={<Register />} />
 
                 <Route element={<ProtectedRoute />}>
-                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/dashboard" element={<Dashboard />} >
+                        <Route index={true} element={<DashboardPage />} />
+                        <Route path="users_page" element={<UsersPages />} />
+                        <Route path="products_page" element={<ProductsPage />} />
+                    </Route>
 
                 </Route>
             </Routes >
